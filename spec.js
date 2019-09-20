c251c8544ac5b9315fa9b7e5a71b920537e9b268
monicca2502 <31113840+monicca2502@users.noreply.github.com>
2019-09-20 02:26:26 +0100

    Added testcases

--- spec.js
+++ spec.js
@@ -4,24 +4,40 @@ var homepage = require('./pageobjects/Homepage');
 
 describe('Duplicate Folder Validation',function(){
   it('should change provider and Apply',async function(){
-    await settingpage.get();
-    await settingpage.ECMSelect();
-    loginpage = await settingpage.ClickApply();
+    await settingpage.getandapply();
+      expect(browser.getCurrentUrl()).toEqual('http://qaexercise.envalfresco.com/login');
   });
-  it('should log in', async function(){  
-    loginpage.setUsername("guest@example.com");
-    loginpage.setPassword("Password");
-    homepage = loginpage.ClickSignIn();
+
+  it('should login with valid credentials', async function(){  
+    await settingpage.getandapply();
+    await loginpage.setUsername("guest@example.com");
+    await loginpage.setPassword("Password");
+    await loginpage.ClickSignIn();
+    expect(browser.getCurrentUrl()).toEqual('http://qaexercise.envalfresco.com/home');
   });
+
+  it('should create new folder', async function(){
+    await settingpage.getandapply();
+    await loginpage.setUsername("guest@example.com");
+    await loginpage.setPassword("Password");
+    await loginpage.ClickSignIn();
+    homepage.createfolder("Alfresco");
+    ((homepage.checkfolder()).then(function (items) {
+    expect(items).isPresent().toBeTruthy();
+    homepage.deletefolder();
+    }));
+
+  });
+
   it('should check duplicate folder creation', async function(){
-    homepage.ClickContenservices();
-    homepage.ClickCreatefoldericon();
-    homepage.EnterNametextbox("Monicca");
-    homepage.ClickCreatebtn();
-    homepage.EnterNametextbox("Monicca");
-    homepage.ClickCreatebtn();
-    expect(homepage.getValidationMessage().toEqual("There's already a folder with this name. Try a different name"));
-    homepage.Threedots();
-    homepage.ClickDeleteoption();    
+    await settingpage.getandapply();
+    await loginpage.setUsername("guest@example.com");
+    await loginpage.setPassword("Password");
+    await loginpage.ClickSignIn();
+    homepage.createfolder("Alfresco");
+        homepage.duplicatefoldercreation("Alfresco");
+        browser.sleep(1000);
+        expect(homepage.newfolderdialogboxPresent()).toBe(true);
+        homepage.deletefolder();
   });
 });
